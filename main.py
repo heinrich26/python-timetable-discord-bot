2d57a8118e3e7fe71db2ff34e875a30c2d1b2268
heinrich26 <65970327+heinrich26@users.noreply.github.com>
2021-08-24 23:36:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-import discord, os
+import discord, os, math
 from timetable_parser import get_replacements, pages
+from itertools import zip_longest
 
 
-empty_field = {'name': '\u200b', 'value': '\u200b', 'inline': False}
+empty_field = {'name': ' ', 'value': ' ', 'inline': False}
 
-def mk_field(name='\u200b', value='\u200b', inline: bool=True) -> dict:
-    return {'name': name if name else '\u200b', 'value': value if value else '\u200b', 'inline': inline}
+def mk_field(name=' ', value=' ', inline: bool=True) -> dict:
+    return {'name': name if name else ' ', 'value': value if value else ' ', 'inline': inline}
+
+def row_for_class(class_a: dict, class_b: dict={}, header: bool = False, has_info: bool=False) -> list:
+    if header:
+        row = [
+            mk_field('**Stunde**', class_a['lesson']),
+            mk_field('**Lehrer**', f"~~{class_a['teacher']}~~{(' ' + class_a['replacing_teacher']) if 'replacing_teacher' in class_a else ''}"),
+            mk_field('**Fach**', class_a['subject']),
+            mk_field('**Raum**', class_a['room']),
+            # hier kommt das Info Feld hin, wenn es eins gibt!
+            mk_field('**Art**', class_a['type_of_replacement'])
+        ]
+        if has_info:
+            table.insert(-1, mk_field('**Info**', class_a['info_text']))
+    else:
+        row = []
+        keys = ('lesson', 'subject', 'room', 'info_text', 'type_of_replacement')
+        for i in range(0, 5):
+            if i == 4 and not has_info: continue
+            key = keys[i]
+            row.append(mk_field(class_a[key], class_b[key] if class_b else None))
+        row.insert(1, mk_field(f"~~{class_a['teacher']}~~{(' ' + class_a['replacing_teacher']) if 'replacing_teacher' in class_a else ''}", None if class_b else f"~~{class_b['teacher']}~~{(' ' + class_b['replacing_teacher']) if 'replacing_teacher' in class_b else ''}"))
+    return row
 
 client = discord.Client()
 
@@ -20,19 +43,44 @@ async def on_message(msg):
 
     text = msg.content
     if text.startswith('/vplan') or text.startswith('/vertretungsplan') or text.startswith('!vplan') or text.startswith('!vertretungsplan'):
-        args = text.split(' ', 1)
+        args = text.split(' ')
+        if len(args) > 1: args[1] = args[1].lower()
+
+        # Vertretungen abfragen
+        replacements = get_replacements()
+        lower_keys = {key.lower(): key for key in replacements} if replacements != None else ()
+
         if len(args) > 2:
             await msg.channel.send('**Ungültige Argumente!**\nVersuch mal `!vplan <Klasse>` oder `!vplan help`!')
             return
-        if len(args) == 2 and args[1] == 'help':
-            help_embed = discord.Embed(title='**Vertretungsplan Hilfe**', description='Hier findest du alle wichtigen Commands für den Vertretungsplan!')
+        elif len(args) == 1: continue
+        elif len(args) == 2 and args[1] == 'help':
+            help_embed = discord.Embed(title='**__Vertretungsplan Hilfe__**', description='Hier findest du alle wichtigen Commands für den Vertretungsplan!')
             help_embed.add_field(name='**Verwendung:** `!vplan [Optionen]`', value='`ohne Args` Zeigt den kompletten Plan\n`... help` Zeigt diese Info\n`... <Klasse>` Zeigt den Plan für eine Klasse\n`... klassen` Zeigt alle Klassen die heute Vertretung haben')
             await msg.channel.send(embed=help_embed)
             return
+        elif args[1] in lower_keys:
+            usr_class: lower_keys[args[1]]
 
+            
+            # Vertretungsplan für eine Klasse
+            embedded_msg = discord.Embed(title=f'Vertretungsplan der {usr_class}',
+                                            description='Hier siehst du deine heutigen Vertretungen')
+            data = sorted(replacements[usr_class], key=lambda e: e['lesson'])
+            no_info = True in [True if 'info_text' in item else None for item in data]
+            fields = row_for_class(data[0], header=True, no_info=no_info)
+            if len(data) != 1:
+                for i in range(0, math.ceil((len(data)-1)/2)):
+                    fields.append(empty_field)
+                    fields.extend(row_for_class(data[i*2+1], data[i*2+2] if i*2+2 != len(data) else None))
+            for field in fields:
+                embedded_msg.add_field(**field)
+
+
+        # Vertretungsplan für alle Klassen
         embedded_msg = discord.Embed(title='Vertretungsplan',
                                     description='Hier siehst du deine heutigen Vertretungen')
-        replacements = get_replacements()
+        
 
         if replacements is None or replacements == {}:
             fields = [{'name': '**Keine Vertretungen heute...**', 'value': '', 'inline': False}]
