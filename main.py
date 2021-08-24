53c45ad17603d902afc6b6b69ef5a4c4b6a4fb16
heinrich26 <65970327+heinrich26@users.noreply.github.com>
2021-08-24 22:17:11 +0200

    Update main.py
    S

--- main.py
+++ main.py
@@ -25,10 +25,8 @@ async def on_message(msg):
             await msg.channel.send('**Ungültige Argumente!**\nVersuch mal `!vplan <Klasse>` oder `!vplan help`!')
             return
         if len(args) == 2 and args[1] == 'help':
-            help_embed = discord.Embed(title='**Vertretungsplan Hilfe**',
-                                        description='Hier findest du alle wichtigen Commands für den Vertretungsplan!')
-            help_embed.add_field('**Verwendung:** `!vplan [Optionen]`',
-                                    '`ohne Args` Zeigt den kompletten Plan\n`... help` Zeigt diese Info\n`... <Klasse>` Zeigt den Plan für eine Klasse\n`... klassen` Zeigt alle Klassen die heute Vertretung haben')
+            help_embed = discord.Embed(title='**Vertretungsplan Hilfe**', description='Hier findest du alle wichtigen Commands für den Vertretungsplan!')
+            help_embed.add_field(name='**Verwendung:** `!vplan [Optionen]`', value='`ohne Args` Zeigt den kompletten Plan\n`... help` Zeigt diese Info\n`... <Klasse>` Zeigt den Plan für eine Klasse\n`... klassen` Zeigt alle Klassen die heute Vertretung haben')
             await msg.channel.send(embed=help_embed)
             return
 
